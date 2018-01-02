e1ea51826901b8156c34e7cd972c02f1d369cd7f
mino-PC\mino <tkmino84@gmail.com>
2018-01-02 16:19:24 +0900

    탭 스크립트 추가

--- html/js/common.js
+++ html/js/common.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
 	resizeMid();
+
+	// 탭 (오버)
+	$('.tab-wrap-over').each(function(){
+		$(this).find('.tab-list .item').mouseenter(function(){
+			$(this).closest('.tab-list').find('.item').removeClass('on');
+			$(this).addClass('on');
+			var idx = $(this).index();
+			$(this).closest('.tab-wrap-over').find('.tab-cont').removeClass('on').eq(idx).addClass('on');
+		});
+	});
+
+	// 탭 (클릭)
+	$('.tab-wrap-click').each(function(){
+		$(this).find('.tab-list .item').click(function(){
+			$(this).closest('.tab-list').find('.item').removeClass('on');
+			$(this).addClass('on');
+			var idx = $(this).index();
+			$(this).closest('.tab-wrap-click').find('.tab-cont').removeClass('on').eq(idx).addClass('on');
+		});
+	});
 });
 
 $(window).load(function(){
